c541708d28c16997b64db86c694a2725c27f50d6
maciejlichon <maciej.lichon@geckodynamics.com>
2023-06-30 10:23:42 +0200

    minor change

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,6 +1,6 @@
 const Container = (props) => {
     return (
-        <div className="max-w-[1110px] mx-auto px-4
+        <div className="box-content max-w-[1110px] mx-auto px-4
             md:px-10"
         >
             {props.children}
